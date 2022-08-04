0b7c3f73435dccbc1a15688fd4ebc52480e2c721
Shreyansh Raj <211230051@nitdelhi.ac.in>
2022-08-04 11:59:29 +0530

    All Basic Algorithms

---
+++ Algo/Kadane.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxSumSubArray(int a[],int n)
+{
+    int csum=a[0],maxSoFar=a[0];
+    for(int i=1;i<n;i++)
+    {
+        csum=max(csum+a[i],a[i]);
+        maxSoFar=max(csum,maxSoFar);
+
+    }
+    return maxSoFar;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    cin>>a[i];
+    cout<<maxSumSubArray(a,n)<<endl;
+    return 0;
+}
